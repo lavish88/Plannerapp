af97b9a4aa5fe8996c201edb5245d32a8cc0fa1f
lavish.madotra <lavish.madotra@amantyatech.com>
2025-05-29 12:46:34 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(200),nullable=False)
+    desc = db.Column(db.String(500),nullable=False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+    
+@app.route('/', methods = ['GET','POST'])
+def hello_world():
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+
+        print(request.form['title'])
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+       
+    allTodo = Todo.query.all()
+    return render_template('index.html', allTodo=allTodo)
+    #return 'Hello, World!'
+
+@app.route('/show')
+def profucts():
+    allTodo = Todo.query.all()
+    print(allTodo)
+    return 'this is product page'
+
+if __name__ == "__main__":
+    app.run(debug=True)
